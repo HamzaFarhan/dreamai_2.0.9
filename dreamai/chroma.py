53a2ab34b3812fe9e045f27e326cf2624b7b65db
Hamza Farhan <thehamza96@gmail.com>
2024-04-04 20:18:45 +0500

    chroma

--- dreamai/chroma.py
+++ dreamai/chroma.py
@@ -41,28 +41,33 @@ def chroma_collection(
 def json_files_to_collection(
     flist: list[str | Path], chroma_collection: ChromaCollection
 ):
-    metas = []
-    ids = [f"id_{uuid4()}" for _ in range(len(flist))]
+    ids = []
     docs = []
-    i = 0
-    for fname in flist:
+    metas = []
+    for i, fname in enumerate(flist):
         with open(fname, "r") as f:
             data = json.load(f)
-            data_str = json.dumps(data)
-            meta = {"level": data["level"], "type": data["type"]}
-            metas.append(meta)
-            docs.append(data_str)
-            i += 1
-    print(f"Ddding {i} files to collection")
-    chroma_collection.add(documents=docs, ids=ids, metadatas=metas)
+            ids.append(f"id_{uuid4()}")
+            metas.append(
+                {
+                    "level": data["level"],
+                    "type": data["type"],
+                    "prev_id": "",
+                    "next_id": "",
+                }
+            )
+            docs.append(json.dumps(data))
+    print(colored(f"Adding {i} files to collection", "cyan"))
+    chroma_collection.add(ids=ids, documents=docs, metadatas=metas)
 
 
 def id_from_lc_doc(doc: LCDocument, doc_idx=None, **kwargs) -> str | None:
-    """Get a unique id from an LCDocument."""
-
     try:
-        file_name = doc.metadata.get("filename", doc.metadata["source"])
-        doc_id = f"{file_name}_{doc.metadata.get('page_number', 0)}"
+        file_name = doc.metadata.get("filename", doc.metadata.get("source"))
+        doc_id = file_name or ""
+        page_number = doc.metadata.get("page_number")
+        if page_number is not None:
+            doc_id += f"_page_{page_number}"
         if doc_idx is not None:
             doc_id += f"_{doc_idx}"
         return doc_id + f"_{uuid4()}"
@@ -72,21 +77,77 @@ def id_from_lc_doc(doc: LCDocument, doc_idx=None, **kwargs) -> str | None:
 
 
 def lc_docs_to_chroma_docs(
-    docs: list[LCDocument], id_fn: Callable = id_from_lc_doc
+    docs: list[LCDocument],
+    id_fn: Callable = id_from_lc_doc,
+    add_links: bool = True,
 ) -> tuple[list[str], list[str], list[dict]]:
-    """Convert a list of LCDocuments to Chroma docs with ids and metadata."""
-
     chroma_ids = []
     chroma_docs = []
     chroma_metadatas = []
     for i, doc in enumerate(docs):
-        chroma_docs.append(doc.page_content)
-        chroma_metadatas.append(
-            {
-                k: v
-                for k, v in doc.metadata.items()
-                if type(v) in [str, int, float, bool]
-            }
-        )
         chroma_ids.append(id_fn(doc=doc, doc_idx=i) or f"id_{i}")
-    return chroma_ids, chroma_docs, chroma_metadatas
+        chroma_docs.append(doc.page_content)
+    for i, doc in enumerate(docs):
+        metadata = {
+            k: v for k, v in doc.metadata.items() if type(v) in [str, int, float, bool]
+        }
+        metadata["prev_id"] = ""
+        metadata["next_id"] = ""
+        if add_links:
+            metadata["prev_id"] = chroma_ids[i - 1] if i > 0 else ""
+            metadata["next_id"] = chroma_ids[i + 1] if i < len(docs) - 1 else ""
+        chroma_metadatas.append(metadata)
+    return {"ids": chroma_ids, "documents": chroma_docs, "metadatas": chroma_metadatas}
+
+
+def traverse_id_tree(
+    metadata: dict,
+    collection: ChromaCollection,
+    direction: str = "prev",
+    n_steps: int = 2,
+) -> list:
+    ids = []
+    curr_id = metadata.get(direction + "_id")
+    for _ in range(n_steps):
+        if not curr_id:
+            break
+        ids.append(curr_id)
+        metadata = collection.get(ids=[curr_id])["metadatas"][0]
+        curr_id = metadata.get(direction + "_id")
+    return ids
+
+
+def query_collection(
+    query_text: str,
+    collection: ChromaCollection,
+    n_results: int = 10,
+    n_traversal_steps: int = 2,
+    include: list[str] = ["metadatas", "documents"],
+) -> list[dict]:
+    query_res = collection.query(
+        query_texts=query_text, n_results=n_results, include=include
+    )
+    if n_traversal_steps == 0:
+        return [
+            {k: [v[0][i]] for k, v in query_res.items() if v is not None}
+            for i in range(n_results)
+        ]
+    results = []
+    for i, metadata in enumerate(query_res["metadatas"][0]):
+        curr_id = query_res["ids"][0][i]
+        prev_ids = traverse_id_tree(
+            metadata=metadata,
+            collection=collection,
+            direction="prev",
+            n_steps=n_traversal_steps,
+        )
+        next_ids = traverse_id_tree(
+            metadata=metadata,
+            collection=collection,
+            direction="next",
+            n_steps=n_traversal_steps,
+        )
+        res_ids = prev_ids[::-1] + [curr_id] + next_ids
+        print(res_ids)
+        results.append(collection.get(ids=res_ids, include=include))
+    return results
